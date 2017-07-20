0edf77db908e05281e2825230a39dbe497ffef5b
Syrus Mesdaghi <syrusm@prominentedge.com>
2017-07-20 17:54:02 -0400

    run service vs ui

---
+++ src/spade-service.js
@@ -0,0 +1,7 @@
+
+
+export default function init() {
+  setInterval(function(){
+    console.log('----> running spade service');
+  }, 1000);
+}
--- tasks/start.js
+++ tasks/start.js
@@ -2,8 +2,16 @@ const childProcess = require('child_process');
 const electron = require('electron');
 const gulp = require('gulp');
 
-gulp.task('start', ['build', 'watch'], () => {
-  childProcess.spawn(electron, ['.'], { stdio: 'inherit' })
+gulp.task('startService', ['build', 'watch'], () => {
+  childProcess.spawn(electron, ['.', '--service'], { stdio: 'inherit' })
+  .on('close', () => {
+    // User closed the app. Kill the host process.
+    process.exit();
+  });
+});
+
+gulp.task('startUi', ['build', 'watch'], () => {
+  childProcess.spawn(electron, ['.', '--ui'], { stdio: 'inherit' })
   .on('close', () => {
     // User closed the app. Kill the host process.
     process.exit();
